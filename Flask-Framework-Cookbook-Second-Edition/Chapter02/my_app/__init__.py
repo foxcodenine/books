262b3b37d54e567e0b1b71ae0bce96a45068e771
Christopher Farrugia <foxcode9@gmail.com>
2020-07-28 21:06:06 +0200

    flask_jinga2_template_filter

--- Flask-Framework-Cookbook-Second-Edition/Chapter02/my_app/__init__.py
+++ Flask-Framework-Cookbook-Second-Edition/Chapter02/my_app/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask 
+from flask import Flask, request
 from my_app.mod.views import hello
 from my_app.mod_product.views import product_blueprint
+
+import ccy
 
 
 
@@ -22,6 +24,17 @@ app.register_blueprint(product_blueprint)
 @app.route('/')
 def index():
     return '{}'.format(app.config['CHECK_ENV'])
+
+# ______________________________________________________________________
+
+# If the filter is at the blueprint level, the decorator would be app_template_filter ;
+# otherwise, at the application level, the decorator would be template_filter;
+
+@app.template_filter('format_currency')
+def format_currency_filter(amount):
+    currency_code = ccy.countryccy(request.accept_languages.best[-2:])
+    return '{} {}'.format(currency_code, amount)
+
 
 # ______________________________________________________________________
 
--- Flask-Framework-Cookbook-Second-Edition/Chapter02/my_app/mod_product/views.py
+++ Flask-Framework-Cookbook-Second-Edition/Chapter02/my_app/mod_product/views.py
@@ -1,11 +1,11 @@
 
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 from my_app.mod_product.models import PRODUCTS
 from werkzeug.exceptions import abort
 
 # ______________________________________________________________________
 
-product_blueprint = Blueprint('product', __name__, url_prefix='/product')
+product_blueprint = Blueprint('product', __name__, url_prefix='/products')
 
 # ______________________________________________________________________
 
@@ -13,6 +13,9 @@ product_blueprint = Blueprint('product', __name__, url_prefix='/product')
 @product_blueprint.route('/')
 @product_blueprint.route('/home/')
 def home():
+    # This will get the best match client language according location 
+    print('>>>>>', request.accept_languages.best)
+
     return render_template('home.html', products = PRODUCTS)
 
 # _______________________
@@ -23,6 +26,14 @@ def product(key):
     product = PRODUCTS.get(key)
     if not product_blueprint:
         abort(404)
-    render_template('product.html', product=product)
+    return render_template('product.html', product=product)
+
 
 # _______________________
+
+# If the filter is at the blueprint level, the decorator would be app_template_filter ;
+# otherwise, at the application level, the decorator would be template_filter;
+
+@product_blueprint.app_template_filter('full_name')
+def full_name_filter(product):
+    return '{0} / {1}'.format(product['category'], product['name'])
