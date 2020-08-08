f4d491cd270a36367a62bde785cc8187112c9258
Christopher Farrugia <foxcode9@gmail.com>
2020-08-08 11:43:42 +0200

    add_functionality_to_app

---
+++ Fullstack_Vue/noties.md
@@ -0,0 +1,7 @@
+
+* Array.prototype.sort()
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
+
+* Array.prototype.find()
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
--- Fullstack_Vue/upvote/main.js
+++ Fullstack_Vue/upvote/main.js
@@ -3,9 +3,25 @@ new Vue({
     data: {
         submissions: Seed.submissions
     },
+    computed: {
+        sortedSubmissions: function(){
+            return this.submissions.sort(function(a, b){
+                return b.votes - a.votes
+            })
+        }
+    },
     methods: {
+        upvote: function(id){
+            
+            const mySubmition = this.submissions.find(function(sub) {
+                return sub.id === id;
+            });
+            
+            mySubmition.votes++;
+        },
         testVue: function() {
             console.log(this.submissions);
         }
+
     }
-})
+});
