b3b62730d5cd1e722d4f3f4d5f9e571bd3b84cce
Christopher Farrugia <foxcode9@gmail.com>
2020-08-18 21:20:31 +0200

    chapter2_calendar_app

---
+++ Fullstack_Vue/calendar_app/src/components/CalendarDay.vue
@@ -0,0 +1,82 @@
+<template>
+  <!--  <div class="day column">
+          <div class="day-banner has-text-white has-text-centered">Mon</div>
+          <div class="day-details">
+            <div class="day-number">1</div>
+            <div class="day-event" style="background-color: rgb(153, 255, 153)">
+              <div>
+                <span class="has-text-centered details">Get Groceries</span> 
+                <div class="has-text-centered icons"><i class="fa fa-pencil-square edit-icon"></i> 
+                  <i class="fa fa-trash-o delete-icon"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> -->
+
+        <div class="day column">
+          <div class="day-banner has-text-white has-text-centered">{{day.abbvTitle}}</div>
+          <div class="day-details">
+            <div class="day-number">{{day.id}}</div>
+
+                <app-calendar-event v-for="(event, index) in day.events" v-bind:key="index" v-bind:event="event" v-bind:day="day">
+
+                </app-calendar-event>
+          </div>
+        </div>
+
+
+</template>
+
+
+<script>
+
+    import CalendarEvent from './CalendarEvent'
+
+    export default {
+        props: ['day'],
+
+        components: {
+            AppCalendarEvent: CalendarEvent
+        }
+    }
+</script>
+
+
+
+
+<style lang="scss" scoped>
+
+    .day {
+    background-color: #4A4A4A;
+    color: #FFF;
+    border-left: 1px solid #8F8F8F;
+    border-bottom: 1px solid #8F8F8F;
+    font-size: 12px;
+    cursor: pointer;
+
+    &:hover {
+      background: darken(#4A4A4A,3%);
+    }
+
+    .day-banner {
+      background-color: #333333;
+      color: #FFF;
+      padding: 10px;
+      text-transform: uppercase;
+      letter-spacing: 1px;
+      font-size: 12px;
+      font-weight: 600;
+    }
+
+    .day-details {
+      padding: 10px;
+
+      
+    }
+
+    &:last-child {
+      border-right: 1px solid #8F8F8F;
+    }
+  }
+</style>
---
+++ Fullstack_Vue/calendar_app/src/components/CalendarEvent.vue
@@ -0,0 +1,57 @@
+<template>
+            <div class="day-event"  :style="getEventBackgroudColor">
+              <div>
+                <span class="has-text-centered details">{{ event.details }}</span> 
+                <div class="has-text-centered icons"><i class="fa fa-pencil-square edit-icon"></i> 
+                  <i class="fa fa-trash-o delete-icon"></i>
+                </div>
+              </div>
+            </div>
+</template>
+
+<script>
+    export default {
+        props: ['event', 'day'],
+        computed: {
+          getEventBackgroudColor() {
+            const color = ['#FF9999', '#85D6FF', '#99FF99']
+            const randomColor = color[Math.floor(Math.random() * color.length)]
+            return `background-color: ${randomColor};`
+          }
+        }
+    }
+</script>
+
+
+
+
+
+<style lang="scss" scoped>
+    .day-event {
+        margin-top: 6px;
+        margin-bottom: 6px;
+        display: block;
+        color: #4C4C4C;
+        padding: 5px;
+
+        .details {
+          display: block;
+        }
+
+        .icons .fa {
+          padding: 0 2px;
+        }
+
+        input {
+          background: none;
+          border: 0;
+          border-bottom: 1px solid #FFF;
+          width: 100%;
+
+          &:focus {
+            outline: none;
+          }
+        }
+      }
+ 
+</style>
