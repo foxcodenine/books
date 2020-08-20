f0b04816a4865b494c7f91bd0f2083c64eb1e29b
Christopher Farrugia <foxcode9@gmail.com>
2020-08-20 21:13:04 +0200

    completed_calandar_app

--- Fullstack_Vue/calendar_app/src/store.js
+++ Fullstack_Vue/calendar_app/src/store.js
@@ -16,8 +16,40 @@ export const store = {
     addEvent(eventDetails) {
         const activeDay = this.getActiveDay();
         activeDay.events.push({details: eventDetails, edit: false});
-    }
+    },
+    findEvent(dayId, details) {
+        
+        const objDay = this.state.seedData.find(day => day.id === dayId);
+        const objEvent = objDay.events.find(event => event.details === details);
+        return objEvent;
+    },
+    editEvent(dayId, details) {        
 
+        const objEvent = this.findEvent(dayId, details);
+        objEvent.edit = true;
+    },
+
+    updateEvent(dayId, details, newEventDetails) {
+
+        const objEvent = this.findEvent(dayId, details);
+        objEvent.details = newEventDetails;
+        objEvent.edit = false;
+    },
+    resetAllEvent() {
+        this.state.seedData.map(dayObj => {
+
+            dayObj.events.map(eventObj => {
+                eventObj.edit = false;
+            });
+        });        
+    },
+    deleteEvent(dayId, eventDetails) {
+
+        const dayObj = this.state.seedData.find((day) => day.id == dayId);
+
+        const eventIndex = dayObj.events.findIndex(event =>  event.details === eventDetails);
+
+        dayObj.events.splice(eventIndex, 1);
+    }
 }
 
-console.log(store);
