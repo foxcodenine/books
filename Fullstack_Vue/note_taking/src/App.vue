b781090bd41b04d2f0dd79cb8bed01b5e5c1365f
Christopher Farrugia <foxcode9@gmail.com>
2020-09-22 21:05:08 +0200

    completed chapter4 introduction to vuex

---
+++ Fullstack_Vue/note_taking/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+
+  <div id="app">
+    <div class="notes-section">
+
+      <div class="columns">
+
+        <div class="column has-text-centered">
+          <strong>Notes</strong>
+          <div class="notes" v-for="(note, index) in notes" :key='index'>
+            {{ note }}
+          </div>
+        </div>
+
+        <div class="column has-text-centered">
+          <strong>Timestamp</strong>
+          <div class="timestamps" v-for="(stamp, index) in timestampes" :key='index'>
+            {{ stamp }}
+          </div>
+        </div>
+
+      </div>
+
+      <app-input-component></app-input-component>      
+    </div>
+    <app-note-count-component></app-note-count-component>
+  </div>
+
+</template>
+
+<script>
+
+  import InputComponent from './components/InputComponent'
+  import NoteCountComponent from "./components/NoteCountComponent";
+
+  export default {
+    name: 'app',
+    components: {
+      appInputComponent: InputComponent,
+      appNoteCountComponent: NoteCountComponent,
+    },
+    computed: {
+      notes() {
+        return this.$store.getters.getNotes;
+      },
+      timestampes() {
+        return this.$store.getters.getTimestamps;
+      }
+    }
+  }
+
+</script>
+
+<style lang="scss">
+</style>
---
+++ Fullstack_Vue/note_taking/src/components/InputComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <input 
+        placeholder='Enter a note' class="input is-small" type="text" 
+        v-model="input" @keyup.enter="monitorEntryKey"/>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        input: '',
+      }
+    },
+    methods: {
+      monitorEntryKey() {
+        this.$store.dispatch('addNote', this.input);
+        this.$store.dispatch('addTimestamp', new Date().toLocaleString());
+        this.input = '';
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
